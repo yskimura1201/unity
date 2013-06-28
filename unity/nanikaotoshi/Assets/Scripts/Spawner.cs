7919b1b1dfd2cdc3bc2134d0756481c9d5ddf8a9
yoshiaki kimura <ys08030102@gmail.com>
2013-06-29 03:38:32 +0900

    toon調整 巨大追加

--- unity/nanikaotoshi/Assets/Scripts/Spawner.cs
+++ unity/nanikaotoshi/Assets/Scripts/Spawner.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Spawner : MonoBehaviour
 {
 	public Transform prefab;
+	public Transform prefab1;
 	public Transform prefab2;
 	public Transform prefab3;
 	
@@ -13,13 +14,15 @@ public class Spawner : MonoBehaviour
      	if (Input.GetButtonDown ("Fire1"))
 		{
 			//Random rand = new Random();
-			int ires = Random.Range (1, 5);
+			int ires = Random.Range (1, 50);
 			Vector3 offset = new Vector3 (0 , 4, 0);
 			transform.rotation = Quaternion.AngleAxis( 180, Vector3.up);
-			if(ires == 1){
+			if(ires < 2){
+				Instantiate (prefab1, transform.position + offset, transform.rotation);
+			}else if(ires < 15){
 				Instantiate (prefab2, transform.position + offset, transform.rotation);
-			}else if(ires == 2){
-				Instantiate (prefab3, transform.position + offset, transform.rotation);	
+			}else if(ires < 30){
+				Instantiate (prefab3, transform.position + offset, transform.rotation);
 			}else{
 				Instantiate (prefab, transform.position + offset, transform.rotation);
 			}
