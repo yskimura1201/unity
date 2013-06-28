95183ec8427feaa8eeab4fd04716b165c8a6650b
yoshiaki kimura <ys08030102@gmail.com>
2013-06-29 02:40:24 +0900

    toon追加

--- unity/nanikaotoshi/Assets/CameraController.cs
+++ unity/nanikaotoshi/Assets/CameraController.cs
@@ -14,6 +14,15 @@ public class CameraController : MonoBehaviour {
 		if(positionY == 1){
 			this.transform.position = new Vector3(0, 6, -8);
 			this.transform.rotation = Quaternion.Euler(20, 0, 0);
+		}else if(positionY == 6){
+			this.transform.position = new Vector3(1, 5, -5);
+			this.transform.rotation = Quaternion.Euler(386, -15, -7);
+		}else if(positionY == 5){
+			this.transform.position = new Vector3(-2, 0, -4);
+			this.transform.rotation = Quaternion.Euler(331, 23, 14);
+		}else if(positionY ==  0){
+			this.transform.position = new Vector3(0, 7, 2);
+			this.transform.rotation = Quaternion.Euler(409, -178, 1);
 		}else{
 			this.transform.position = new Vector3(0, 1, -9);
 			this.transform.rotation = Quaternion.Euler(340, 0, 0);
--- unity/nanikaotoshi/Assets/Spawner.cs
+++ unity/nanikaotoshi/Assets/Spawner.cs
@@ -5,6 +5,7 @@ public class Spawner : MonoBehaviour
 {
 	public Transform prefab;
 	public Transform prefab2;
+	public Transform prefab3;
 	
 	// Update is called once per frame
 	void Update ()
@@ -14,8 +15,11 @@ public class Spawner : MonoBehaviour
 			//Random rand = new Random();
 			int ires = Random.Range (1, 5);
 			Vector3 offset = new Vector3 (0 , 4, 0);
+			transform.rotation = Quaternion.AngleAxis( 180, Vector3.up);
 			if(ires == 1){
 				Instantiate (prefab2, transform.position + offset, transform.rotation);
+			}else if(ires == 2){
+				Instantiate (prefab3, transform.position + offset, transform.rotation);	
 			}else{
 				Instantiate (prefab, transform.position + offset, transform.rotation);
 			}
